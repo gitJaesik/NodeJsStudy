b05865858e41ca03bc32b952d0c5ef5b9209c123
gitJaesik <maguire1815@gmail.com>
2016-10-03 23:54:32 +0900

    board basic complete

--- node_mini_server/board/index.js
+++ node_mini_server/board/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
-var bodyparser = require('body-parser');
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/html');
+
 
 // const는 수정 x, var는 옛날 것, let은 블록 안에서 사용하면 사용 후 바로 소멸됨
 let conn = mysql.createConnection({
@@ -20,15 +24,69 @@ let conn = mysql.createConnection({
 });
 
 conn.connect();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use([cookieParser(), session({secret:'jaesik'})]);
+
+app.use((req,res,next) => {
+	//console.log(req.cookies);
+	//console.log(req.session);
+	next();
+});
+
+app.post('/login', (req,res) => {
+	if (!req.body.userid || !req.body.password) {
+		res.status(422).end("Unprocessable Entity");
+	}else{
+		conn.query("select * from users where userid=? and password=?", [
+			req.body.userid, req.body.password], (err, rows, field) => {
+				if (err) {
+					res.status(500).end();
+				}
+
+				if (rows.length == 0) {
+					//res.status(401).end("Not Authorized");
+					res.redirect('/?error=login');
+				}
+				else {
+					req.session.member = rows[0];
+					res.redirect('/');	
+				}
+				
+				//req.settion
+			});
+	}
+});
+
+app.post('/logout', (req,res) => {
+	delete req.session.member;
+	res.redirect('/');
+});
+
 app.get('/', (req,res)=> {
 	var query = conn.query("SELECT * FROM users ORDER BY id DESC", function(err,rows){
         //console.log(rows);
         // res.json(rows);
 		res.render("index.ejs", {
-			rows : rows
+			member : req.session.member,
+			rows : rows,
+			error : req.query.error
 		});
 
     });
+});
+
+app.get('/write', (req,res) => {
+	if (!req.session.member) {
+		res.redirect('/');
+	}
+	else {
+		res.render("write.ejs", {
+			member : req.session.member,
+			error : req.query.error
+		});
+	}
 });
 
 app.post('/', (req,res)=> {
