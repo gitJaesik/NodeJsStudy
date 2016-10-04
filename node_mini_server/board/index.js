c9f455a81c5bec01be911ad74c382806fca22c59
gitJaesik <maguire1815@gmail.com>
2016-10-04 20:18:25 +0900

    use promise and make CRUD default

--- node_mini_server/board/index.js
+++ node_mini_server/board/index.js
@@ -39,7 +39,7 @@ app.post('/login', (req,res) => {
 	if (!req.body.userid || !req.body.password) {
 		res.status(422).end("Unprocessable Entity");
 	}else{
-		conn.query("select * from users where userid=? and password=?", [
+		conn.query("select id, name, userid, role from users where userid=? and password=?", [
 			req.body.userid, req.body.password], (err, rows, field) => {
 				if (err) {
 					res.status(500).end();
@@ -59,14 +59,20 @@ app.post('/login', (req,res) => {
 	}
 });
 
-app.post('/logout', (req,res) => {
+app.get('/logout', (req,res) => {
 	delete req.session.member;
 	res.redirect('/');
 });
 
 app.get('/', (req,res)=> {
-	var query = conn.query("SELECT * FROM users ORDER BY id DESC", function(err,rows){
-        //console.log(rows);
+
+
+	// var query = conn.query("SELECT * FROM users ORDER BY id DESC", function(err,rows){
+/*
+	var query = conn.query("SELECT users.id as usernumber, users.name, users.userid, " +
+		"articles.id as articleid, articles.title, articles.text " + 
+		"FROM users LEFT JOIN articles on articles.userid = users.id ORDER BY articles.id DESC;", function(err,rows){
+        //console.log(rows); 
         // res.json(rows);
 		res.render("index.ejs", {
 			member : req.session.member,
@@ -75,6 +81,61 @@ app.get('/', (req,res)=> {
 		});
 
     });
+*/
+
+	var promiseQueryArticleInfo = function() {
+		return new Promise(function(resolve, reject) {
+			var query = conn.query("SELECT users.id as usernumber, users.name, users.userid, " +
+			"articles.id as articleid, articles.title, articles.text " + 
+			"FROM users JOIN articles on articles.userid = users.id ORDER BY articles.id DESC;", function(err, rows) {
+				if (err) {
+					reject(Error(err));
+				}
+				else {
+					resolve(rows);
+				}
+			});
+		});
+	};
+
+	var promiseQueryUserInfo = function() {
+		return new Promise(function(resolve, reject) {
+			var query = conn.query("SELECT * FROM users ORDER BY id DESC", function(err, rows) {
+				if (err) {
+					reject(Error(err));
+				}
+				else {
+					resolve(rows);
+				}
+			});
+		});
+	};
+
+	Promise.all([promiseQueryArticleInfo(), promiseQueryUserInfo()]).then( function(values) {
+		// console.log(values);
+		res.render("index.ejs", {
+			member : req.session.member,
+			rowsForArticles : values[0],
+			rowsForUsers : values[1],
+			error : req.query.error
+		});
+	},function(err) {
+		console.log(err);
+	});
+
+	/*
+	promiseQueryUserInfo().then(
+		function(val) {
+			res.render("index.ejs", {
+				member : req.session.member,
+				rows : val,
+				error : req.query.error
+			});
+		}
+	);
+	*/
+
+
 });
 
 app.get('/write', (req,res) => {
@@ -82,10 +143,100 @@ app.get('/write', (req,res) => {
 		res.redirect('/');
 	}
 	else {
+
+		// 게시판 내용 저장하기 
+		var old = null;
+		if (req.session.flash) {
+			old = req.session.flash;
+			delete req.session.flash;
+		}
 		res.render("write.ejs", {
 			member : req.session.member,
-			error : req.query.error
+			error : req.query.error,
+			old : old
 		});
+	}
+});
+
+app.post('/write', (req,res) => {
+	if (!req.session.member) {
+		res.redirect('/');
+	}
+	else {
+		//console.log(req.body);
+		if (!req.body.title || !req.body.content) {
+			req.session.flash = {title:req.body.title, content:req.body.content};
+			res.redirect('/write?error=write');
+		}
+		else {
+			var query = conn.query("INSERT INTO articles (userid, title, text) VALUES (?, ?, ?)",
+				[req.session.member.id, req.body.title, req.body.content], function(err, result) {
+					if (err) {
+						console.log(err);
+						res.status(400).send('insert write value error');
+					}
+					else {
+						res.redirect('/');
+					}
+				});
+		}
+	}
+
+});
+
+app.get('/update/:id', (req,res)=> {
+
+	var promiseQueryArticleInfo = function() {
+		return new Promise(function(resolve, reject) {
+			var query = conn.query("SELECT users.id as usernumber, users.name, users.userid, " +
+			"articles.id as articleid, articles.title, articles.text " + 
+			"FROM users JOIN articles on articles.userid = users.id WHERE articles.id = (?) ORDER BY articles.id DESC;", req.params.id, function(err, rows) {
+				if (err) {
+					reject(Error(err));
+				}
+				else {
+					resolve(rows);
+				}
+			});
+		});
+	};
+
+	promiseQueryArticleInfo().then( (value) => {
+		res.render("update.ejs", {
+			member : req.session.member,
+			error : req.query.error,
+			rows : value[0]
+		});}, (err)=> {
+			if (err) {
+				console.log(err);
+			}
+		});
+	//res.status(400).send('test');
+});
+
+app.post('/update/:id', (req,res)=> {
+
+	if (!req.session.member) {
+		res.redirect('/');
+	}
+	else {
+		//console.log(req.body);
+		if (!req.body.title || !req.body.content) {
+			req.session.flash = {title:req.body.title, content:req.body.content};
+			res.redirect('/update?error=update');
+		}
+		else {
+			console.log(req.body.title + ":" + req.body.content + ":" + req.params.id);
+			var query = conn.query("UPDATE articles SET articles.title = (?), articles.text = (?) WHERE articles.id = (?)",
+				[req.body.title, req.body.content, req.params.id], function(err, result) {
+				if (err) {
+					console.log(err);
+				}else
+				{
+					res.redirect('/');
+				}
+			});
+		}
 	}
 });
 
